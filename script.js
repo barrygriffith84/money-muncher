87f2183139755173fef3a534ae3e999707b4267a
Barry Griffith <barrygriffith@outlook.com>
2020-05-24 18:55:25 -0400

    Everything is positioned with CSS.  Now checks the user input to see if they're correct.

--- script.js
+++ script.js
@@ -7,26 +7,18 @@ TODO:
 -Consider making a logo using a money-related font from Google Fonts
 */
 
+//520 x 612
 let coinCount = 0;
 let moneyTotal = 0;
 let coinValue = 0;
-let coinType = ""
-
+let coinId = ""
+let userGuess = 0;
 //Determines which coin was consumed and reprints that coin to coin-div
-const printCoin = (coin) => {
-  if (coin == "penny") {
-    document.querySelector("#coin-div").innerHTML +=
-      `<img src="images/penny.png" alt="penny" id="penny-1" class="coin" draggable="true" ondragstart="drag(event)">`
-  } else if (coin == "nickel") {
-    document.querySelector("#coin-div").innerHTML +=
-      `<img src="images/nickel.png" alt="nickel" id="nickel-5" class="coin" draggable="true" ondragstart="drag(event)">`
-  } else if (coin == "dime") {
-    document.querySelector("#coin-div").innerHTML +=
-      `<img src="images/dime.png" alt="dime" id="dime-10" class="coin" draggable="true" ondragstart="drag(event)">`
-  } else if (coin == "quarter") {
-    document.querySelector("#coin-div").innerHTML +=
-      `<img src="images/quarter.png" alt="quarter" id="quarter-25" class="coin" draggable="true" ondragstart="drag(event)">`
-  }
+const printCoin = (coinId) => {
+console.log(coinId)
+  const coin = coinId.split("-")[0]
+  document.querySelector("#coin-div").innerHTML +=
+      `<img src="images/${coin}.png" alt="${coin}" id="${coinId}" class="coin" draggable="true" ondragstart="drag(event)">`
 
 }
 
@@ -37,15 +29,29 @@ const allowDrop = (ev) => {
 const drag = (ev) => {
   ev.dataTransfer.setData("text", ev.target.id);
   coinValue = ev.target.id.split("-").pop();
-  coinType = ev.target.id.split("-")[0];
+  coinId = ev.target.id
 }
 
 const drop = (ev) => {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   document.querySelector(`#${data}`).remove();
-  printCoin(coinType);
+  printCoin(coinId);
   coinCount++;
   moneyTotal += eval(coinValue);
   console.log(moneyTotal / 100);
 }
+
+const checkUser = () => {
+  if(userGuess == moneyTotal/100){console.log("You are right")}
+  else{
+    console.log("WRONG")
+  }
+}
+
+document.querySelector("#submit-btn").addEventListener("click", () => {
+  
+  userGuess = document.querySelector("#user-input").value 
+  console.log(userGuess)
+  checkUser();
+})
